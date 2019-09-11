4487c5eb21c15a311eba56249ca56f1046ca33b2
teppei_gym <yamamutaisou@gmail.com>
2019-09-11 14:31:06 +0900

    'pushしたものを取り消す実験'

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,6 +1,7 @@
 @extends ('layouts.app')
 @section ('content')
 
+test
 <h1 class="page-header">{{ $user->name }}のToDo一覧</h1>
 <p class="text-right">
   <a class="btn btn-success" href="/todo/create">新規作成</a>
