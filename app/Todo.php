63040cf6e2bf01fb2ebc7ea5b1b5876c06387fdc
teppei_gym <yamamutaisou@gmail.com>
2019-09-11 13:45:05 +0900

    Revert "'論理削除の処理を追記'"
    This reverts commit aa348ce6188fc93bb95fd8896fec91eb720a2ecb.

--- app/Todo.php
+++ app/Todo.php
@@ -3,11 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 class Todo extends Model
 {
-    use SoftDeletes;
-    protected $dates = ['deleted_at'];
+    //
     protected $fillable = [
         'title',
         'user_id'
